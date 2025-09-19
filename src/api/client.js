c950c83c15136960ffcdd7a566993f4d34af9212
profabhishek <jhaabhishek910@gmail.com>
2025-09-19 14:41:52 +0530

    check

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL ?? "https://834f4a5aff79.ngrok-free.app/api",
 });
