ec40055674391d11002c43973ac3f176c7033ed8
profabhishek <jhaabhishek910@gmail.com>
2025-09-20 00:30:20 +0530

    push

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? "https://834f4a5aff79.ngrok-free.app/api",
+  baseURL: import.meta.env.VITE_API_URL ?? "http://coreex-env.eba-mfhecmbg.ap-south-1.elasticbeanstalk.com",
 });
